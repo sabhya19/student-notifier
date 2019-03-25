7b474e2d0bf426d1ee9c6f9e57bac026ed3e6b0d
vedaant-rajoo <vedaant12345@gmail.com>
2019-03-25 21:26:38 +0530

    removed unnecessary imports

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, flash , redirect, url_for, session, logging, request
-from flask_mysqldb import MySQL
-from passlib.hash import sha256_crypt
 from wtforms import Form, StringField, PasswordField, validators
 
 
